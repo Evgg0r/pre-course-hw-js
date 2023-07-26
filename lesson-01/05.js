0772d69435c910a52ca46c751b101853b35f198c
evgg0r <gor.evg2100@gmail.com>
2023-07-26 23:42:38 +0700

    commit

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -5,10 +5,9 @@ switch (rule) {
         console.log('Все таки нет правил без исключения'); 
         break
     case rule.length > 25: 
-        console.log('Все таки нет правил без исключения'); 
+        console.log('У меня 100 пудов все получится'); 
         break;
     default:
         console.log('50 на 50')
-    
     }
 
