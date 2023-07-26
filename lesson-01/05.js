570cbd309c0f6c9914175e1c5a7e7dc090595e9f
evgg0r <gor.evg2100@gmail.com>
2023-07-26 23:30:21 +0700

    comit 3

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,7 +1,6 @@
 let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
-console.log(ryle.length)
 
-switch (rule) {
+switch (trule) {
     case rule.length < 25: 
         console.log('Все таки нет правил без исключения'); 
         break
