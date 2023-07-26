c4150c82ea03f6439cdb93ba28e9dbf0cb0835df
evgg0r <gor.evg2100@gmail.com>
2023-07-27 00:01:00 +0700

    commit

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,9 +1,9 @@
 let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
 
-switch (rule) {
+switch (true) {
     case rule.length < 25: 
         console.log('Все таки нет правил без исключения'); 
-        break
+        break;
     case rule.length > 25: 
         console.log('У меня 100 пудов все получится'); 
         break;
