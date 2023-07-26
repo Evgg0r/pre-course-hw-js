37e0e573ca44aa4e34abfb878f99615d165f1f98
evgg0r <gor.evg2100@gmail.com>
2023-07-26 23:25:32 +0700

    homework 5

---
+++ lesson-01/05.js
@@ -0,0 +1,15 @@
+let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
+console.log(ryle.length)
+
+switch (rule) {
+    case rule.length < 25: 
+        console.log('Все таки нет правил без исключения'); 
+        break
+    case rule.length > 25: 
+        console.log('Все таки нет правил без исключения'); 
+        break;
+    default:
+        console.log('50 на 50')
+    
+    }
+
