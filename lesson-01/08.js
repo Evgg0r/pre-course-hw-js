cbec0894b83d2c4f1fa2158401432841bb6fba58
evgg0r <gor.evg2100@gmail.com>
2023-07-31 23:20:20 +0700

    commit

---
+++ lesson-01/08.js
@@ -0,0 +1,34 @@
+const sum = (= )
+const copyPrep = (prep) => {
+if (dayOfWeek === "Saturday" || dayOfWeek === "Sunday") { 
+    dayOfWeek = "dayOff" 
+} else { 
+    dayOfWeek = "weekday" a
+} 
+ 
+switch (dayOfWeek) { 
+    case "weekday": 
+        console.log("Будний день"); 
+        break; 
+ 
+    case "dayOff": 
+        console.log("Выходной день"); 
+        break; 
+    default: 
+        console.log("Некорректное значение дня недели");
+
+console.log(100)
+
+
+let prep = { 
+    name: "Igor", 
+    sizes: { 
+        height: 177, 
+        weight: 80 
+    } 
+}; 
+
+
+//Напишите функцию copyPrep, которая принимает один аргумент - объект prep, и возвращает новый объект, который является копией исходного объекта prep, в котором все тоже самое, но также появилось IsMarried: true . Используйте иммутабельный подход для создания копии объекта, включая его вложенные свойства.
+
+} 
