5c3039819f719620bc47c397b1d908c1c75df427
evgg0r <gor.evg2100@gmail.com>
2023-09-23 11:29:16 +0700

    COMIT

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -1,4 +1,4 @@
-let passportMarried = {
+et passporNottMarried = {
     name: "Petr",
     surname: "Petrov",
     address: {
@@ -7,7 +7,12 @@ let passportMarried = {
     }
 };
 
-let passportMarried2 = {...passportMarried, married: true};
-
+let passportMarried = {
+    ...passporNottMarried,
+    address: {
+    ...passporNottMarried.address
+    }
+};
+passportMarried.married = true;
+console.log(passporNottMarried); 
 console.log(passportMarried);
-console.log(passportMarried2);
